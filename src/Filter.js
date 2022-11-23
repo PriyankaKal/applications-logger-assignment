d1d0f7d4760aa67f774eb8675e474b63b21625d3
Priyanka K <priyanka.kalmakar@neosoftmail.com>
2022-11-23 15:03:28 +0530

    assignment - logger table application

---
+++ src/Filter.js
@@ -0,0 +1,177 @@
+import React, { useState, useEffect } from 'react'
+import DatePicker from "react-datepicker";
+
+
+import "react-datepicker/dist/react-datepicker.css";
+
+
+const Filter = ({data, props, sort, order,setSearchResults}) => {
+  const [startDate, setStartDate] = useState(null);
+    const [endDate, setEndDate] = useState(null);
+
+  const [startdate, setstartdate] = useState(new Date());
+  const [enddate, setenddate] = useState(new Date());
+  const [searchInput, setSearchInput] = useState('');
+
+  const [dateFilter, setDateFilter] = useState({
+    startDate: null,
+    endDate: null
+  })
+  
+    //called when a user selects filter start-date 
+    const handleStartDate = (date) => {
+      setStartDate(date);
+  }
+
+  //called when a user selects filter end-date 
+  const handleEndDate = (date) => {
+      setEndDate(date);
+  }
+
+  const handleFilterByDate = () => {
+    if (startdate && enddate) {
+      
+      const filtereddatedata = data.filter([startdate, enddate]);
+      console.log(filtereddatedata,"filtereddatedata");
+    }
+}
+
+
+  const applyFilter = () => {
+  
+    if (startdate && enddate ) {
+     
+        handleFilterByDate();
+        
+    }
+ 
+   
+    if (!startdate && enddate || startdate && !enddate) {
+        window.alert("Please Make sure you select start-date and end-date");
+    }
+}
+  
+  const searchItems = (searchValue) => {
+    setSearchInput(searchValue)
+    if (searchInput !== '') {
+        const filteredData = data.filter((item) => {
+            return Object.values(item).join('').toLowerCase().includes(searchInput.toLowerCase())
+        })
+        console.log(filteredData,"filterdata");
+        setSearchResults(filteredData)
+    }
+    else{
+      console.log(data,"data");
+        setSearchResults(data)
+    }
+}
+
+    return(
+<div>
+
+<div class="container-fluid">
+  
+      <div class="row" style={{textAlign:"center"}}>
+       
+        <h3>
+    Logger search
+  </h3>
+
+          </div>
+        <hr></hr>  
+          </div>
+  
+<form>
+    <div class="container-fluid">
+      <div class="row">
+      
+        <div class="col-sm-2">
+        <label>
+           <p>Action Type</p>
+           <input name="action_type" onChange={(e) => searchItems(e.target.value)}
+                        />
+           {/* <select name="action_type" style={{
+                    paddingTop:"9px"}}>
+  
+                    <option></option>
+                    {data.map(info => {
+                        return <option key={info.actionType == null ? "-/-"  : info.actionType.replace( /_/g, " " )} >
+                            {info.actionType == null ? "-/-"  : info.actionType.replace( /_/g, " " )}
+                        </option>
+                    })}
+                   
+                </select> */}
+         </label>
+       
+        </div>
+        <div class="col-sm-2">
+        <label>
+           <p>Application Type</p>
+           <input name="application_type" onChange={(e) => searchItems(e.target.value)}
+                        />
+           {/* <select name="application type" style={{
+                    paddingTop:"9px"}}>
+  
+                    <option></option>
+                    {data.map(info => {
+                        return <option key={info.applicationType == null ?  "----": info.applicationType.replace( /_/g, " " )} >
+                            {info.applicationType == null ?  "----": info.applicationType.replace( /_/g, " " )}
+                        </option>
+                    })}
+                   
+                </select> */}
+         </label>
+       
+        </div>
+        <div class="col-sm-2">
+        <label>
+           
+           <p className="startDate">From: </p> <div className="datePickerLabel">{startDate ? startDate.toLocaleDateString("fr-CA") : null}</div>
+           <DatePicker selected={startdate} onChange={(date) => setstartdate(date)} handleDateChange={handleStartDate} date={startDate} />
+           
+   
+         </label>
+       
+        </div>
+        <div class="col-sm-2">
+        <label>
+           
+           <p className="todate" >To:</p> <div className="datePickerLabel">{endDate ? endDate.toLocaleDateString("fr-CA") : null}</div>
+           <DatePicker selected={enddate} onChange={(date) => setenddate(date)} handleDateChange={handleEndDate} date={endDate} />
+    
+         </label>
+       
+        </div>
+        <div class="col-sm-2">
+        <label>
+           <p>Application ID</p>
+           <input name="application_id" onChange={(e) => searchItems(e.target.value)}
+                        />
+         </label>
+       
+        </div>
+        <div class="col-sm-2">
+        <label>
+          
+           <button type="button" class="btn btn-primary" onClick={applyFilter} style={{
+                    marginTop:"38px"}}>Submit Logger</button>
+         </label>
+       
+        </div>
+       
+      </div>
+      <br></br>
+      
+      <br></br>
+    </div>
+   
+  
+  
+ 
+</form>
+</div>
+    );
+
+    
+}
+export default Filter;
---
+++ src/InnerLogTable.js
@@ -0,0 +1,118 @@
+import React, { useState, useEffect } from 'react'
+import Table from 'react-bootstrap/Table';
+import Arrow from "./images/downArrow.png"
+
+
+const InnerLogTable = ({data, sort, order,searchResults}) => {
+
+    return (  
+        <div class="continer-fluid">
+<div class="row">
+<Table class="table table-bordered">
+            <thead>
+                <tr>
+                <th>Log Id <span onClick={() => sort('logId')}>{order == 'asc' ?  <img src={Arrow} alt="sort" style={{transform:"rotate(0deg)",
+                    height:"10px"
+                }} ></img> : <img src={Arrow} alt="sort" style={{
+                    transform:"rotate(180deg)",
+                    height:"10px"
+                }} ></img>}</span>
+                </th>
+                <th>Application Type <span onClick={() => sort('appType')}>{order == 'asc' ?  <img src={Arrow} alt="sort" style={{
+                    transform:"rotate(0deg)",
+                    height:"10px"
+                }} ></img> : <img src={Arrow} alt="sort" style={{
+                    transform:"rotate(180deg)",
+                    height:"10px"
+                }} ></img>}</span></th>
+                <th>Application Id<span onClick={() => sort('applicationId')}>{order == 'asc' ?  <img src={Arrow} alt="sort" style={{
+                    transform:"rotate(0deg)",
+                    height:"10px"
+                }} ></img> : <img src={Arrow} alt="sort" style={{
+                    transform:"rotate(180deg)",
+                    height:"10px"
+                }} ></img>}</span></th>
+                
+                <th>Action<span onClick={() => sort('action')}>{order == 'asc' ?  <img src={Arrow} alt="sort" style={{
+                    transform:"rotate(0deg)",
+                    height:"10px"
+                }} ></img> : <img src={Arrow} alt="sort" style={{
+                    transform:"rotate(180deg)",
+                    height:"10px"
+                }} ></img>}</span></th>
+                <th>Action Details</th>
+                <th>Date : Time<span onClick={() => sort('date')}>{order == 'asc' ?  <img src={Arrow} alt="sort" style={{
+                    transform:"rotate(0deg)",
+                    height:"10px"
+                }} ></img> : <img src={Arrow} alt="sort" style={{
+                    transform:"rotate(180deg)",
+                    height:"10px"
+                }} ></img>}</span></th>
+    
+                </tr>
+            </thead>
+            <tbody>
+            {searchResults.length > 0 ? (
+               
+                searchResults.map((info => (
+                        
+                            <tr>
+                              <td>{info.logId}</td>
+                              <td>{info.applicationType == null ? "-/-"  : info.applicationType.replace( /_/g, " " )}</td>
+                        <td>{info.applicationId == null ? "-/-"  : info.applicationId}</td>
+                        
+                        <td>{info.actionType == null ? "-/-"  : info.actionType.replace( /_/g, " " )}</td>
+                        <td>-/-</td>
+                        <td>{info.creationTimestamp}</td>
+                            </tr>
+                        )))
+                ) : (
+                data.map(info => (
+                    <tr>
+                      <td>{info.logId}</td>
+                      <td>{info.applicationType == null ? "-/-"  : info.applicationType.replace( /_/g, " " )}</td>
+                <td>{info.applicationId == null ? "-/-"  : info.applicationId}</td>
+                
+                <td>{info.actionType == null ? "-/-"  : info.actionType.replace( /_/g, " " )}</td>
+                <td>-/-</td>
+                <td>{info.creationTimestamp}</td>
+                    </tr>
+                )))
+            }
+
+            </tbody>
+        </Table>
+</div>
+
+        </div>
+        
+      ) 
+
+                }
+          export default InnerLogTable;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
